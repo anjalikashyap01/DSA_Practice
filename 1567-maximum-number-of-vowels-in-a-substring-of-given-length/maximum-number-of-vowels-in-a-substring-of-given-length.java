5f251a49f75aea6f3c253e0d5cc5d7c39f6ce3e6
anjalikashyap01 <kashyapanjali2005@gmail.com>
2026-03-10 15:43:25 +0530

    Time: 22 ms (25.36%) | Memory: 46.8 MB (16.22%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,34 +1,39 @@
 class Solution {
-    boolean is_vowel(char c)
-    {
-        return(c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
-    }
     public int maxVowels(String s, int k) {
-        int n=s.length();
         int max_count=0;
+        int start=0;
+        int end=0;
+        int n=s.length();
         int count=0;
-        for(int i=0;i<k;i++)
+        for(end=0;end<k;end++)
         {
-            if(is_vowel(s.charAt(i)))
+            if(s.charAt(end)=='a'||s.charAt(end)=='e'||s.charAt(end)=='i'||s.charAt(end)=='o'||s.charAt(end)=='u')
             {
                 count++;
             }
+        }
+            max_count=count;
+            
+            while(end<n)
+            {
+                if(end>=k && (s.charAt(start)=='a'||s.charAt(start)=='e'||s.charAt(start)=='i'||s.charAt(start)=='o'||s.charAt(start)=='u'))
+                {
+                    count--;
+                }
+                start++;
+            
+            if(s.charAt(end)=='a'||s.charAt(end)=='e'||s.charAt(end)=='i'||s.charAt(end)=='o'||s.charAt(end)=='u')
+            {
+                count++;
+            }
+        if(count>max_count)
+        {
             max_count=count;
         }
-        for(int i=k;i<n;i++)
-        {
-            if(is_vowel(s.charAt(i-k)))
-            {
-                count--;
-            }
-            if(is_vowel(s.charAt(i)))
-            {
-                count++;
-            }
-             max_count=Math.max(max_count,count);
-        }
+            
+        end++;
        
-        return max_count;
-
+    }
+     return max_count;
     }
 }
